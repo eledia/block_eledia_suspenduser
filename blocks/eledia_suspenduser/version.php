8cc6df2b30e098f7fb05365ae1415e769669b015
Matthias Schwabe <matthias.schwabe@eledia.de>
2018-06-15 17:46:14 +0200

    eledia_suspenduser - incremented plugin version

--- blocks/eledia_suspenduser/version.php
+++ blocks/eledia_suspenduser/version.php
@@ -24,8 +24,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2013101500;// The current plugin version (Date: YYYYMMDDXX).
-$plugin->release = '0.1 (2012051600)';
+$plugin->version = 2018061500;// The current plugin version (Date: YYYYMMDDXX).
+$plugin->release = '0.2 (2018061500)';
 $plugin->component= 'block_eledia_suspenduser';
 $plugin->cron = 86400;
 $plugin->maturity = MATURITY_STABLE;
